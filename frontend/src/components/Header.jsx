caafcee3070a859a020777491921d31af5ce3e3d
ömer <omeryazar0808@gmail.com>
2023-06-21 00:11:40 +0300

    add login-logout func.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,37 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { logout, reset } from "../features/auth/authSlice"
 
 const Header = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { user } = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
   return (
     <header className="bg-slate-50 flex items-center justify-between p-5 shadow">
         <div>
             <Link to='/' className="font-bold text-2xl">CalenderApp</Link>
         </div>
         <ul className="flex items-center gap-5">
-            <li className="px-3 py-1 font-bold">
-                <Link to='/login'>Login</Link>
-            </li>
-            <li className="px-3 py-1 font-bold">
-                <Link to='/register'>Register</Link>
-            </li>
+            {user ? (
+                <li className="px-3 py-1 font-bold">
+                    <button onClick={onLogout}>Logout</button>
+                </li>) : (
+                <>
+                    <li className="px-3 py-1 font-bold">
+                        <Link to='/login'>Login</Link>
+                    </li>
+                    <li className="px-3 py-1 font-bold">
+                        <Link to='/register'>Register</Link>
+                    </li>
+                </>)}
+            
         </ul>
     </header>
   )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
 
 const Login = () => {
 
@@ -8,6 +12,24 @@ const Login = () => {
     })
   
     const {email, password} = formData
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        if(isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
 
 
     const onChangeHandler = (e) => {
@@ -25,7 +47,7 @@ const Login = () => {
           password
         }
   
-        // dispatch(login(userData))
+        dispatch(login(userData))
     }
 
 
